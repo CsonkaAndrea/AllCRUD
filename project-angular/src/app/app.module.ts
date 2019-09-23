af6424d9ec773bc52c0f5607bd1180419a8eff17
skth <evelin.buth@gmail.com>
2019-09-23 18:38:14 +0200

    FEAT: order status - first try

--- project-angular/src/app/app.module.ts
+++ project-angular/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +10,8 @@ import { ProductsComponent } from './components/admin/products/products.componen
 import { OrdersComponent } from './components/admin/orders/orders.component';
 import { IndexComponent } from './components/admin/index/index.component';
 import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { UpdateOrdersComponent } from './components/admin/orders/update-orders/update-orders.component';
+import { OrderStatusComponent } from './components/admin/orders/order-status/order-status.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +20,15 @@ import { DashboardComponent } from './components/admin/dashboard/dashboard.compo
     ProductsComponent,
     OrdersComponent,
     IndexComponent,
-    DashboardComponent
+    DashboardComponent,
+    UpdateOrdersComponent,
+    OrderStatusComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- project-angular/src/app/components/admin/orders/orders.component.ts
+++ project-angular/src/app/components/admin/orders/orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from 'src/app/services/orders.service';
+import { Order } from 'src/app/models/order/order';
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private ordersService: OrdersService) {
+  };
 
+  orders: Order[];
+  updateCustomer: Order = new Order();
+
+  // Read all orders onInit
   ngOnInit() {
-  }
+    this.ordersService.getAll().subscribe(orders => {
+      this.orders = orders
+    });
+  };
 
 }
