31005fcc9401de7182863e684a060be257ed5e6b
Csonka-Kis Andrea <cskand@gmail.com>
2019-09-23 12:56:11 +0200

    dashboard component created

--- project-angular/src/app/app.module.ts
+++ project-angular/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/layout/header/header.component';
 import { ProductsComponent } from './components/admin/products/products.component';
 import { OrdersComponent } from './components/admin/orders/orders.component';
 import { IndexComponent } from './components/admin/index/index.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { IndexComponent } from './components/admin/index/index.component';
     HeaderComponent,
     ProductsComponent,
     OrdersComponent,
-    IndexComponent
+    IndexComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
