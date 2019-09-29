f83c36dd44e8674499bb92b76245076651fe567b
Neowyne <krisztina.skravan@gmail.com>
2019-09-30 01:54:52 +0200

    Feat: dashboard frame with customers, prod and orders to follow based on it

--- project-angular/src/app/components/admin/dashboard/dashboard.component.ts
+++ project-angular/src/app/components/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomersService } from 'src/app/services/customers.service';
+import { OrdersService } from 'src/app/services/orders.service';
+import { ProductService } from 'src/app/services/product.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private customers: CustomersService, private orders: OrdersService, private product: ProductService) { }
+
+  allCustomer: any
+  allCustomer$: Observable<any> = this.customers.getAll()
+  customersNumber: number = 0
+
 
   ngOnInit() {
+    this.customers.getAll().subscribe(
+      sajt => this.allCustomer = sajt
+    )
+    console.log('All Customer', this.allCustomer)
+
+    //Count customers
+    for (let i = 0; i < this.allCustomer.length; i++) {
+      this.customersNumber++
+    }
   }
 
 }
