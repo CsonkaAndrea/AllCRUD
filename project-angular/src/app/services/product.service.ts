c022ce5ee9e01c2a6e0905b3e378ef24dcd380a0
skth <evelin.buth@gmail.com>
2019-09-30 16:31:43 +0200

    validation

--- project-angular/src/app/services/product.service.ts
+++ project-angular/src/app/services/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Product } from '../models/product/product';
 
 @Injectable({
@@ -10,11 +10,12 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
+  // list: BehaviorSubject<any> = new BehaviorSubject([]);
   url: string = 'http://localhost:3000/api/products';
 
   getAll(): Observable<any> {
     return this.httpClient.get(this.url);
-  }
+   }
 
   getOne(id: number): Observable<any> {
     return this.httpClient.get(`${this.url}/${id}`);
