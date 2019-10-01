66a70a4887507f41d71099ba1f4405cfbcf1d3a3
Katkó György <georgekomor@gmail.com>
2019-10-01 11:46:16 +0200

    basket design, team page

---
+++ project-express/routes/theTeam.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.render('theTeam', {
+        title: 'The Team'
+    })
+});
+
+module.exports = router;
