dfa07aac558a5868d2859c59971a0a758ec3cdc9
Gergely Tóth <gergogergo91@gmail.com>
2019-10-02 16:48:02 +0200

    FEAT: basket counter works

--- project-express/middleware/basketCounter.js
+++ project-express/middleware/basketCounter.js
@@ -3,17 +3,18 @@ var router = express();
 const BasketLogic = require('./../modules/basketLogic');
 const basketLogic = new BasketLogic();
 
+router.use(async (req, res, next) => {
+  if (req.user) {
+    // Get logged in user's basker ID
+    let loggedInUserBasket = await basketLogic.getBasketId(req);
+    let basketID = loggedInUserBasket.id;
+    // Count products in basket
+    let basketItemCounter = await basketLogic.countBasketItems(basketID);
+    let user = req.user;
+    user.prodsInBasket = basketItemCounter[0].sumOfProds;
+    req.user = user;
+  };
+  next();
+});
 
-/* router.use((req, res, next) => {
-
-}) */
-
-/* 
-  // Get logged in user's basker ID
-  let loggedInUserBasket = await basketLogic.getBasketId(req);
-  let basketID = loggedInUserBasket.id;
-  // Count products in basket
-  let basketItemCounter = await basketLogic.countBasketItems(basketID);
-  user.prodsInBasket = basketItemCounter[0].sumOfProds;
- */
 module.exports = router;
