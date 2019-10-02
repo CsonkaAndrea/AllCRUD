327642201bf383d1f5b5af3f7771fdf0f0870b8b
Neowyne <krisztina.skravan@gmail.com>
2019-10-02 12:07:48 +0200

    Update: commit before rebase

--- project-angular/src/app/pipes/order-by-date.pipe.ts
+++ project-angular/src/app/pipes/order-by-date.pipe.ts
@@ -1,22 +1,22 @@
-import { Pipe, PipeTransform } from '@angular/core';
+// import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({
-  name: 'orderByDate'
-})
-export class OrderByDatePipe implements PipeTransform {
+// @Pipe({
+//   name: 'orderByDate'
+// })
+// // export class OrderByDatePipe implements PipeTransform {
 
-  transform(value: [], key: string): any {
-    if (value) {
-      value.sort((a: any, b: any) => {
-        return ((new Date(a[key]) - new Date(b[key])) * (-1))
-      });
-    };
-    return value;
-  }
-}
+// //   transform(value: [], key: string): any {
+// //     if (value) {
+// //       value.sort((a: any, b: any) => {
+// //         return ((new Date(a[key]) - new Date(b[key])) * (-1))
+// //       });
+// //     };
+// //     return value;
+// //   }
+// // }
 
-//Ezzel át lehetne alakítani, hogy emberi dáum jelenjen meg...
-/*   value.forEach(data => {
-    console.log(new Date(data[key]).getFullYear());
-  }
-  ) */
+// //Ezzel át lehetne alakítani, hogy emberi dáum jelenjen meg...
+// /*   value.forEach(data => {
+//     console.log(new Date(data[key]).getFullYear());
+//   }
+//   ) */
