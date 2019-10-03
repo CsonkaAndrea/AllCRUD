8e14f005ccf9d5c6efe8d8af7e4f8f748b6075a3
Neowyne <krisztina.skravan@gmail.com>
2019-10-03 10:44:33 +0200

    Feat: dashboard displays values properly

--- project-angular/src/app/components/admin/dashboard/dashboard.component.ts
+++ project-angular/src/app/components/admin/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CustomersService } from 'src/app/services/customers.service';
 import { OrdersService } from 'src/app/services/orders.service';
 import { ProductService } from 'src/app/services/product.service';
-import { Observable } from 'rxjs';
+import { zip } from 'rxjs';
 import { AdminsService } from 'src/app/services/admins.service';
 
 @Component({
@@ -11,10 +11,6 @@ import { AdminsService } from 'src/app/services/admins.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  // customers$: Observable<any> = this.customers.getAll();
-  // orders$: Observable<any> = this.orders.getAll();
-  // products$: Observable<any> = this.product.getAll();
-  // admins$: Observable<any> = this.admin.getAll();
 
   adminsArray: any;
   customersArray: any;
@@ -27,82 +23,25 @@ export class DashboardComponent implements OnInit {
   allCustomer: number = 0;
   allOrders: number = 0;
   allProducts: number = 0;
+  allUsers: number = 0;
 
   constructor(private customers: CustomersService, private orders: OrdersService, private product: ProductService, private admin: AdminsService) {
-    // this.countUsers();
-
-    // this.countProducts();
-    // console.log('Products: ', this.products$);
   }
 
   ngOnInit() {
-    // this.countUsers()
-    // this.countOrders()
-    this.product.getAll().subscribe(data => {
-      this.productsArray = data;
-      console.log('43 ', this.productsArray);
-      for (let i = 0; i < this.productsArray.length; i++) {
-        this.allProducts++
-      }
-      console.log('47', this.allProducts);
+
+    zip(
+      this.admin.getAll(),
+      this.customers.getAll(),
+      this.orders.getAll(),
+      this.product.getAll()
+    ).forEach(data => {
+      this.allAdmins = data[0].length;
+      this.allCustomer = data[1].length;
+      this.allOrders = data[2].length;
+      this.allProducts = data[3].length;
+      this.allUsers = this.allAdmins + this.allCustomer;
     });
 
-    this.orders.getAll().subscribe(data => {
-      this.ordersArray = data;
-      console.log('52', this.ordersArray);
-      for (let i = 0; i < this.ordersArray.length; i++) {
-        this.allOrders++
-      };
-      console.log('56', this.allOrders);
-      // this.countOrders(this.allOrders);
-    });
-
-    this.admin.getAll().subscribe(data => {
-      this.adminsArray = data;
-      console.log('62', this.adminsArray);
-      for (let i = 0; i < this.adminsArray.length; i++) {
-        this.allAdmins++
-      };
-      console.log('66', this.allAdmins);
-      // this.countOrders(this.allOrders);
-    });
-
-    this.orders.getAll().subscribe(data => {
-      this.ordersArray = data;
-      console.log('52', this.ordersArray);
-      for (let i = 0; i < this.ordersArray.length; i++) {
-        this.allOrders++
-      };
-      console.log('59', this.allOrders);
-      // this.countOrders(this.allOrders);
-    });
   }
-
-  // countProducts() {
-  //   for (let i = 0; i < this.productsArray.length; i++) {
-  //     this.allProducts++
-  //   }
-  //   console.log('73', this.allProducts);
-  // }
-
-  // countUsers() {
-  //   for (let custNumber in this.customers$) {
-  //     this.countCustomers++
-  //   }
-
-  //   for (let adminNumber in this.admins$) {
-  //     this.countAdmins++
-  //   }
-
-  //   this.allCustomer = this.countCustomers + this.countAdmins
-  //   return this.allCustomer
-  // }
-
-  // countOrders(numberVariable) {
-  //   for (let orderNumber in this.orders$) {
-  //     this.allOrders++
-  //   }
-  //   console.log('All Orders: ', numberVariable);
-  // }
-
 }
