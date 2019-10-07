72615bca8f53e222c9a1d762152fecd563321607
Csonka-Kis Andrea <cskand@gmail.com>
2019-10-07 15:54:19 +0200

    bugfix

---
+++ project-express/models/modelAuthentication.js
@@ -0,0 +1,10 @@
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    user: 'root',
+    password: 'root',
+    database: 'webshop' // database name
+});
+
+module.export = modelAuthentication {
+
+}
